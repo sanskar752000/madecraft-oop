3ba220cc92acdb5dd9aeedb3c74433bc0cd8bbb6
Sanskar Gupta <sanskar752000@gmail.com>
2021-01-09 01:13:06 +0530

    Changes commited on Cat and Harry Class

--- 2. Inside Classes/exercises/Cat.java
+++ 2. Inside Classes/exercises/Cat.java
@@ -30,14 +30,36 @@ public class Cat {
 			System.out.println("DEAD CAT :(");
 	}
 
+	@Override
+	public String toString() {
+		return "Cat name is " + name + " and she has" + lives + " lives";
+	}
+
 	public static void main(String[] args) {
 		/* Do the following things without changing the Cat class */
 
 		// 1. Make the Cat meow
+		Cat sillyCat = new Cat("Silly");
+		sillyCat.meow();
+		sillyCat.meow();
+		sillyCat.meow();
+
 		
 		// 2. Get the Cat to print it's name
+		// this can one way to print the cat name...
+		sillyCat.printName();
+
+		//this can be another way to print the name and additional info...
+		System.out.println(sillyCat);
 
 		// 3. Kill the Cat!
+
+		sillyCat.kill();
+		sillyCat.kill();
+		sillyCat.kill();
+		sillyCat.kill();
+		sillyCat.kill();
+
 
 	}
 }
--- 2. Inside Classes/exercises/Harry.java
+++ 2. Inside Classes/exercises/Harry.java
@@ -27,10 +27,18 @@ public class Harry {
 
 	public static void main(String[] args) {
 		// 1. make harry potter
+
+		Harry harryPotter = new Harry();
 		// 2. become invisible
+		harryPotter.makeInvisible(true);
 		// 3. spy on professor snape
+
+		harryPotter.spyOnSnape();
 		// 4. become visible again
+		harryPotter.makeInvisible(true);
 		// 5. cast a “stupefy” spell
+
+		harryPotter.castSpell("Vingadium levioooousaa");
 	}
 
 }
