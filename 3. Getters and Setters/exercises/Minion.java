2461d2c03ce09025b8d1e051e037b170a1d808d8
Sanskar Gupta <sanskar752000@gmail.com>
2021-01-10 18:48:09 +0530

    excercise done with creating getter and setter method for mininon class

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -1,5 +1,35 @@
 package exercises;
 
 public class Minion {
+    private String name;
+    private int eyes;
+    private String color;
+    private String master;
 
+    public Minion(String name, int eyes, String color, String master) {
+        this.name = name;
+        this.eyes = eyes;
+        this.color = color;
+        this.master = master;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getEyes() {
+        return this.eyes;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getMaster() {
+        return this.master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
 }
